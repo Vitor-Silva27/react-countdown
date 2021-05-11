662e6437e7210d2004918abcef3863c2a0dfc2c9
Vitor Silva <developer.vitorsilva@gmail.com>
2021-05-10 22:31:07 -0300

    fixed timer bug

--- src/components/Cards/CardsContainer/index.js
+++ src/components/Cards/CardsContainer/index.js
@@ -3,21 +3,23 @@ import Card from "../Card";
 import { Cards } from "./style";
 
 const CardsContainer = () => {
-  const [days, setDays] = useState(8);
-  const [hours, setHours] = useState(23);
-  const [minutes, setMinutes] = useState(55);
-  const [seconds, setSeconds] = useState(41);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(1);
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(10);
 
   const addZero = (value) => (value < 10 ? "0" + value : value);
 
   React.useEffect(function countdown(){
     const countInterval = setInterval(() => {
-      setSeconds(seconds - 1);
+      if(!(days == 0 && hours == 0 && minutes == 0 && seconds == 0)){
+        setSeconds(seconds - 1);
+      }
     }, 1000);
     
     if (seconds <= 0 && minutes > 0) {
       setMinutes(minutes - 1);
-      setSeconds(60);
+      setSeconds(59);
     }
     if (minutes <= 0 && hours > 0) {
       setHours(hours - 1);
@@ -26,9 +28,6 @@ const CardsContainer = () => {
     if (hours <= 0 && days > 0) {
       setDays(days - 1);
       setHours(23);
-    }
-    if (days <= 0) {
-      return;
     }
     return () => clearInterval(countInterval);
   }, [seconds]);
